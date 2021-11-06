d7b15c1aea0c9d684c5c14b9b31e7ac6f34d8260
ygtdev <51426246+ygtdev@users.noreply.github.com>
2021-11-07 01:38:57 +0300

    Create

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+var app = document.getElementById("app")
+
+app.innerHTML+=``
+
+var themeButton = document.getElementById("theme-button")
+themeButton.onclick = function(){
+    alert("Theme switching is temporarily disabled.");
+}
