664b87a48a4f99539b47d0bcbd684b47147149a0
COOL <yigitgurses2012@gmail.com>
2023-10-12 23:34:29 +0300

    Create

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen w-full flex items-center justify-center">
+        <div class="flex flex-col items-center gap-y-4">
+            <h1 class="text-4xl text-slate-200 font-bold">
+                I hope I'll do something in the future
+            </h1>
+            <h1 class="text-slate-400">ygtdev.com</h1>
+        </div>
+    </div>
+</template>
+
+<style>
+body {
+    font-family: 'Chakra Petch', sans-serif;
+    background-color: #0f172a;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+      head: {
+          charset: 'utf-8',
+          viewport: 'width=device-width, initial-scale=1',
+          link: [
+              {
+                  rel: 'stylesheet',
+                  href: 'https://pro.fontawesome.com/releases/v6.0.0-beta3/css/all.css'
+              },
+              {
+                  rel: 'preconnect',
+                  href: 'https://fonts.googleapis.com'
+              },
+              {
+                  rel: 'preconnect',
+                  href: 'https://fonts.gstatic.com'
+              },
+              {
+                  rel: 'stylesheet',
+                  href: 'https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
+              }
+          ]
+      }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
+})
