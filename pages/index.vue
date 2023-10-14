936f7c992250d92ad91c63641819c81a29a3dded
COOL <yigitgurses2012@gmail.com>
2023-10-15 01:59:25 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,20 @@
                     A full-stack software developer from Turkey.
                 </p>
             </div>
+            <div
+                class="w-80 p-4 flex items-center gap-x-4 rounded-xl border border-slate-800"
+            >
+                <div class="h-14 w-14 bg-teal-400 rounded-md"></div>
+                <div class="flex-1 flex flex-col gap-y-2">
+                    <div>
+                        <h1 class="text-xs text-slate-400">BLOK3</h1>
+                        <h1 class="text-slate-200">N'aptığını bilmesem de</h1>
+                    </div>
+                    <div class="h-1 bg-slate-800 rounded-full overflow-hidden">
+                        <div class="h-full w-1/3 bg-teal-400"></div>
+                    </div>
+                </div>
+            </div>
             <div class="flex gap-x-4">
                 <a
                     href="https://github.com/ygtdev"
